6933e99de9b0c8f89861029f67c3c227238569a8
jianing_dai <752442633@qq.com>
2022-06-14 09:41:57 +0800

    6.14 start

--- README.md
+++
@@ -1,4 +0,0 @@
-# a-calLearn
-这里是一个HTML的简单计算器程序，主要是我用来练习web基础和js代码用的。
-还没有写完。
-to be continued
---
+++ php/test.php
@@ -0,0 +1,12 @@
+<?php
+/*$i = date("s");
+$s = 0;
+while($s<5){
+    $s++;
+    ob_flush();
+    flush();
+    echo date("Y-m-d H:i:s")."<br>";
+    $i=date("s");
+    sleep(1);
+}*/
+
