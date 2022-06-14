2b001e238456c71cdd28c73ff5341fe66e064976
jianing_dai <752442633@qq.com>
2022-06-14 12:09:57 +0800

    6/14 2022 12:10

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # a-calLearn
 小白自己学习用的
+主要包含php，html，JavaScript等网页网络相关语言技能的学习
--- php/test.php
+++ php/test.php
@@ -1,6 +1,7 @@
 <?php
 /*$i = date("s");
 $s = 0;
+date_default_timezone_set( 'Asia/Shanghai' );
 while($s<5){
     $s++;
     ob_flush();
@@ -8,5 +9,26 @@ while($s<5){
     echo date("Y-m-d H:i:s")."<br>";
     $i=date("s");
     sleep(1);
-}*/
+}
+function calc($a){
+    echo "the result is :$a <br>";
+}
+calc(100);*/
+function re_01($pattern, $source)
+{
+    $result = preg_match($pattern, $source);
+    if ($result) {
+        echo "匹配成功<br>";
+    } else {
+        echo "匹配失败<br>";
+    }
+}
+
+function re_phone(){
+    return $pattern="/^1[3-9]\d{9}$/";
+}
+function re_ip($source)
+{
+    return $pattern = "/(^[01]?\d?\d$)|(^2[0-4]$)\d|(^25[0-5]$)\.(^[01]?\d?\d$)|(^2[0-4]$)\d|(^25[0-5]$)\.(^[01]?\d?\d$)|(^2[0-4]$)\d|(^25[0-5]$)\.(^[01]?\d?\d$)|(^2[0-4]$)\d|(^25[0-5]$)/";
+}
 
